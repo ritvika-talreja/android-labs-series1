d1b87be918f8f83a786e9f48243bb8dcf44aa625
Ritvika Talreja <155548993+ritvika-talreja@users.noreply.github.com>
2024-08-17 17:51:41 +0530

    Add files via upload

---
+++ Lab1Greeting/app/src/main/java/com/example/lab1greeting/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.lab1greeting;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
